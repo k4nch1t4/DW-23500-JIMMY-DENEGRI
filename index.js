6a6da01cf994c6245387854b78d6ad32824ec1e3
Jimmy Denegri <92012891+k4nch1t4@users.noreply.github.com>
2022-01-13 13:46:43 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const navToggle = document.querySelector(".menu__nav__toggle");
 const navList = document.querySelector(".menu__nav__list");
 
 navToggle.addEventListener("click", () => {
-    navList.classList.toggle(".menu__nav__list-visible");
+    navList.classList.toggle("visible");
 });
